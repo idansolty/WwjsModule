5d0c1695dbab90d2c3975646121f0c0d09e01531
Ariel Safar <ariel1safar@gmail.com>
2022-12-09 10:21:24 +0200

    Add CR comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BotModule } from './bot/bot.module';
+// CR: Export all the bots from a single index file and then import bots from some 'index.ts'. then use this import as the array of the providers 
 import { WhatsappBot } from './WwjsClient/proxy/server';
 
 @Module({
