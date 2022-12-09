b23478c6ec5e120419a544957c0bb88554ec1433
Ariel Safar <ariel1safar@gmail.com>
2022-12-09 10:00:36 +0200

    Basic CR comments

--- src/WwjsClient/common/interfaces/BotController.ts
+++ src/WwjsClient/common/interfaces/BotController.ts
@@ -1,8 +1,13 @@
+/**
+ * CR: 
+ * Remove unused imports
+ */
 import { WhatsappBot } from "src/WwjsClient/proxy/server";
 import { Chat, GroupChat, GroupNotification, Message, Reaction } from "whatsapp-web.js";
 import { AuthOperationType, dataForAuth, GenericControllerAuth, POSSIBLE_AUTHS } from "../auths/auth.enum";
 
 export class BotController {
+    // CR: list and not List
     public Lists: Record<string, string[]>;
     public blackList;
     private argsForAuth;
@@ -38,6 +43,7 @@ export class BotController {
         this[functionChoosen.methodName](args[0]);
     }
 
+    // CR: chooseFunction should be private 
     public chooseFunction(functions, ...args) {
         const message = args[0];
 
@@ -46,6 +52,7 @@ export class BotController {
         return functionChoosen;
     }
 
+    // CR: authify should be private 
     public async authify(functionName, auths, ...args): Promise<boolean> {
         const message: any = args[0];
         const relevantAuths = auths.filter(currentAuth => currentAuth.methodName === functionName.methodName);
@@ -68,16 +75,23 @@ export class BotController {
         return authResult;
     }
 
+    // CR: addArgsForAuth should be private, and i think you're not using this function 
     public addArgsForAuth(name: string, value: any) {
         this.argsForAuth[name] = value;
     }
 
+    // CR: This function is unused
     private addAuthObjects(authType: string, operation: (options: dataForAuth) => boolean) {
         const newAuthObject = new AuthOperationType(authType, operation);
 
         this.authsFunctions.addAuth(newAuthObject)
     }
 
+    /**
+     * CR: 
+     * Add boolean return type
+     * What is the type of 'value'?
+     */
     public addToList(name: string, value) {
         if (this.Lists[name]) {
             this.Lists[name].push(value);
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { WhatsappBot } from './WwjsClient/proxy/server';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CR: Export all the bots from a single index file and then import bots from some 'index.ts'. Then use foreach on the array of bots and start all of them
   const bot = app.get(WhatsappBot);
 
   bot.start(app)
